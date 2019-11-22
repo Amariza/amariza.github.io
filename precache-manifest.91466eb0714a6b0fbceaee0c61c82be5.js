5c4304b0ac432bcc5c5b8570d5b6c56bf132b08b
Fabian Lopez <lombareload@gmail.com>
2019-11-22 09:38:23 -0500

    primer commit

---
+++ precache-manifest.91466eb0714a6b0fbceaee0c61c82be5.js
@@ -0,0 +1,334 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84e72bdc25a4411ad0b439500a97e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54461c1dd7da643149dd",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "32a9c2a1297ea53db09f",
+    "url": "/static/css/main.e6fefec5.chunk.css"
+  },
+  {
+    "revision": "54461c1dd7da643149dd",
+    "url": "/static/js/2.ce209fd3.chunk.js"
+  },
+  {
+    "revision": "32a9c2a1297ea53db09f",
+    "url": "/static/js/main.7a0a2e6a.chunk.js"
+  },
+  {
+    "revision": "d8e7ed39979e0811ffe1",
+    "url": "/static/js/runtime-main.773f8f65.js"
+  },
+  {
+    "revision": "55dcf64e2e62aaebf4b94ef42b3df63a",
+    "url": "/static/media/Balance general.55dcf64e.png"
+  },
+  {
+    "revision": "12a9602229a031e4bc38594f2243ff86",
+    "url": "/static/media/Capital de trabajo1.12a96022.png"
+  },
+  {
+    "revision": "1736d913fe99be26b1fa45bd51421b24",
+    "url": "/static/media/Clasificación y Tipos1.1736d913.jpg"
+  },
+  {
+    "revision": "346b52fcb2bf0cebbfbfa03ca233993e",
+    "url": "/static/media/Estados financieros estáticos y dinámicos y su forma de presentación1.346b52fc.png"
+  },
+  {
+    "revision": "8c6e2637a2ed37525af7298b5b4d3145",
+    "url": "/static/media/Estudio Economico-Financiero1.8c6e2637.png"
+  },
+  {
+    "revision": "d12d86a074cc484d5296b612640a46be",
+    "url": "/static/media/Estudio Económico-Financiero1.1.d12d86a0.jpg"
+  },
+  {
+    "revision": "ef477723b8dfee853d95e63e973a7da9",
+    "url": "/static/media/Estudio Económico-Financiero1.1.ef477723.png"
+  },
+  {
+    "revision": "745cc849caf396b30d6ac1983a92b016",
+    "url": "/static/media/Fondo-superior.745cc849.png"
+  },
+  {
+    "revision": "3916e40cac3fcd5dcc007c2055454f1a",
+    "url": "/static/media/Imagen1.3916e40c.jpg"
+  },
+  {
+    "revision": "116e2a94da0a443ea056b1ec5da3393e",
+    "url": "/static/media/Imagen10.116e2a94.jpg"
+  },
+  {
+    "revision": "f68ca47facd8f1b714ff45212d9873e1",
+    "url": "/static/media/Imagen11.f68ca47f.jpg"
+  },
+  {
+    "revision": "37c417621c78e2263c2c435ba43c4529",
+    "url": "/static/media/Imagen12.37c41762.jpg"
+  },
+  {
+    "revision": "31152121c6c1f57d16522d39399f0110",
+    "url": "/static/media/Imagen13.31152121.jpg"
+  },
+  {
+    "revision": "44e38e79c25df64b9214901d5cc078d8",
+    "url": "/static/media/Imagen14.44e38e79.jpg"
+  },
+  {
+    "revision": "3d3d067139ce55b8941c2e73e0f1bbb5",
+    "url": "/static/media/Imagen15.3d3d0671.jpg"
+  },
+  {
+    "revision": "9fefa575172e70bada40a11dd8ded8f6",
+    "url": "/static/media/Imagen16.9fefa575.jpg"
+  },
+  {
+    "revision": "caa40788752b7053c97b01046d2edf45",
+    "url": "/static/media/Imagen17.caa40788.jpg"
+  },
+  {
+    "revision": "0fd8dcd94dd4d70b326ad8b2c0c87393",
+    "url": "/static/media/Imagen18.0fd8dcd9.jpg"
+  },
+  {
+    "revision": "b3fef6f7f1be2d1a3606d2fe542a7e1c",
+    "url": "/static/media/Imagen19.b3fef6f7.jpg"
+  },
+  {
+    "revision": "aaeb750fecc3795a7282961efe81cfc5",
+    "url": "/static/media/Imagen2.aaeb750f.png"
+  },
+  {
+    "revision": "e3f77e91bdc0353aa7d8207454998992",
+    "url": "/static/media/Imagen2.e3f77e91.jpg"
+  },
+  {
+    "revision": "3e4f9353e19488f84b73b7396a726298",
+    "url": "/static/media/Imagen20.3e4f9353.jpg"
+  },
+  {
+    "revision": "e6224162a2e94da37e157c54e6f86b3b",
+    "url": "/static/media/Imagen21.e6224162.jpg"
+  },
+  {
+    "revision": "e5436d278baa4fe06e39bbfb19edd7c0",
+    "url": "/static/media/Imagen22.e5436d27.jpg"
+  },
+  {
+    "revision": "c0c1c97797764a0b2b81981aca9b9036",
+    "url": "/static/media/Imagen3.c0c1c977.png"
+  },
+  {
+    "revision": "ed3d023d0b4e0f3f1adfe4d38222ceac",
+    "url": "/static/media/Imagen3.ed3d023d.jpg"
+  },
+  {
+    "revision": "74b9d25c5c1c0f20a3b3a3713c2c62a5",
+    "url": "/static/media/Imagen4.74b9d25c.jpg"
+  },
+  {
+    "revision": "90822f406f72d34104632002f62e7c3e",
+    "url": "/static/media/Imagen4.90822f40.png"
+  },
+  {
+    "revision": "21f955fc86005f90690e2deeb66f8fa2",
+    "url": "/static/media/Imagen5.21f955fc.png"
+  },
+  {
+    "revision": "b7b6965d39209920b45bc06abfe1742f",
+    "url": "/static/media/Imagen5.b7b6965d.jpg"
+  },
+  {
+    "revision": "04f672bbea8eb02e1bd95df36c0e754b",
+    "url": "/static/media/Imagen6.04f672bb.jpg"
+  },
+  {
+    "revision": "54a956c7801268f83b94831a1d9a0a62",
+    "url": "/static/media/Imagen6.54a956c7.jpg"
+  },
+  {
+    "revision": "669487e2156ed6c808ed8b116a226fbb",
+    "url": "/static/media/Imagen6.669487e2.png"
+  },
+  {
+    "revision": "72dd53faaaf5da72145c61d3bd8e7041",
+    "url": "/static/media/Imagen7.72dd53fa.jpg"
+  },
+  {
+    "revision": "92a943eb0fa16e7c265c1a8111dee049",
+    "url": "/static/media/Imagen7.92a943eb.jpg"
+  },
+  {
+    "revision": "da23b6d12d6f219fbdc256b01b2f1aed",
+    "url": "/static/media/Imagen7.da23b6d1.png"
+  },
+  {
+    "revision": "02c5262cafc85bd5b2628dc5d3d9b887",
+    "url": "/static/media/Imagen8.02c5262c.jpg"
+  },
+  {
+    "revision": "1f1c5c53f5ce28b32b8d7d51c2214ca7",
+    "url": "/static/media/Imagen8.1f1c5c53.jpg"
+  },
+  {
+    "revision": "2becc5bd8d7fd43ebea966bfcb662d4a",
+    "url": "/static/media/Imagen8.2becc5bd.png"
+  },
+  {
+    "revision": "9deb9c696e646dc34f0cff80f7b28357",
+    "url": "/static/media/Imagen9.9deb9c69.png"
+  },
+  {
+    "revision": "c2b66037582b9398559f57246af43078",
+    "url": "/static/media/Imagen9.c2b66037.jpg"
+  },
+  {
+    "revision": "cb641bc809e84a3a2ba6809b877b28b1",
+    "url": "/static/media/Imagen9.cb641bc8.jpg"
+  },
+  {
+    "revision": "fee775d52b16364611a13beaf9752e82",
+    "url": "/static/media/Inversión total1.fee775d5.png"
+  },
+  {
+    "revision": "89ad7c05047bd3a2d1b8b38c722e6b02",
+    "url": "/static/media/La inflación1.89ad7c05.png"
+  },
+  {
+    "revision": "eb8dae79cf214730c7cc37e6b5010187",
+    "url": "/static/media/Logo.eb8dae79.svg"
+  },
+  {
+    "revision": "7bcb3043a4b8bd975158bf82ad73fc93",
+    "url": "/static/media/Razones de cobertura.7bcb3043.jpg"
+  },
+  {
+    "revision": "b3b948c3d35aba13f9642ddb9d184ec2",
+    "url": "/static/media/Razones de endeudamiento.b3b948c3.jpg"
+  },
+  {
+    "revision": "cdaca9ec3e19fb55c6c6540056895601",
+    "url": "/static/media/Razones de liquidez1.cdaca9ec.jpg"
+  },
+  {
+    "revision": "4de20820a56015a671ffed1ee2926a62",
+    "url": "/static/media/Razones de liquidez2.4de20820.jpg"
+  },
+  {
+    "revision": "a986a41eb8744a7333062ca9ea7d222d",
+    "url": "/static/media/Razones de rentabilidad.a986a41e.jpg"
+  },
+  {
+    "revision": "edbc5b0419da059f56db4cd5c3bbe70f",
+    "url": "/static/media/Razones financieras.edbc5b04.jpg"
+  },
+  {
+    "revision": "f3f7b6ad5efc5c7b374bcf29450650c1",
+    "url": "/static/media/evaluación económica y financiera1.1.f3f7b6ad.png"
+  },
+  {
+    "revision": "f3b4da4b1e90d2dea3d070d116c596bc",
+    "url": "/static/media/evaluación económica y financiera1.2.f3b4da4b.png"
+  },
+  {
+    "revision": "204e786b4913b7c18afabced0eee4596",
+    "url": "/static/media/evaluación económica y financiera1.204e786b.jpg"
+  },
+  {
+    "revision": "74be6a6266ce54f31c24db0f4660792d",
+    "url": "/static/media/ilustracion9.74be6a62.jpg"
+  },
+  {
+    "revision": "ebdda3884400b1386377ef4e804ccd17",
+    "url": "/static/media/imagen1.ebdda388.png"
+  },
+  {
+    "revision": "ef0891f673eea1ad746565dc4a2de0fb",
+    "url": "/static/media/imagen1.ef0891f6.png"
+  },
+  {
+    "revision": "1a36b3b11a407022e9dffe45c6eec6d8",
+    "url": "/static/media/imagen10.1a36b3b1.png"
+  },
+  {
+    "revision": "aab1d3d290a94d85df146ca83a1a9117",
+    "url": "/static/media/imagen2.aab1d3d2.png"
+  },
+  {
+    "revision": "0aa011ed84bef251e07d9602ecd2f4d6",
+    "url": "/static/media/imagen3.0aa011ed.png"
+  },
+  {
+    "revision": "6da2684afb6f4bc2d726dcf7544f9541",
+    "url": "/static/media/imagen4.6da2684a.png"
+  },
+  {
+    "revision": "df9a73387b29b9df97e7d2fa28f8b1aa",
+    "url": "/static/media/imagen5.df9a7338.png"
+  },
+  {
+    "revision": "3506e866c9185f5f7bcdb2fc700b8d0d",
+    "url": "/static/media/imagen6.3506e866.png"
+  },
+  {
+    "revision": "e07fe64da736d7ec7478cd48bd1e7b87",
+    "url": "/static/media/imagen7.e07fe64d.png"
+  },
+  {
+    "revision": "7f93db929669dc45d57d92ddc94e0222",
+    "url": "/static/media/imagen8.7f93db92.png"
+  },
+  {
+    "revision": "8d588e7c60ef28251ac737cb166a5b76",
+    "url": "/static/media/imagen9.8d588e7c.png"
+  },
+  {
+    "revision": "7a847ce274e53461c77134839637afb9",
+    "url": "/static/media/integrantes-proyecto-individual.7a847ce2.jpg"
+  },
+  {
+    "revision": "f0a5edb11b5d94feb7ce7897904c4285",
+    "url": "/static/media/proyectos_inversion_3.f0a5edb1.jpg"
+  },
+  {
+    "revision": "850d2324a1b1f6d89b15661456cbd314",
+    "url": "/static/media/proyectos_inversion_sociales.850d2324.jpg"
+  },
+  {
+    "revision": "18f694a00f412cbd8f4f30e3261fba32",
+    "url": "/static/media/tasa efectiva 1.18f694a0.png"
+  },
+  {
+    "revision": "66711880d519a0b0465f0721c5000b14",
+    "url": "/static/media/tasas-interes-real.66711880.jpg"
+  },
+  {
+    "revision": "4c639dcab27a96a09e39e7b46edf4f0b",
+    "url": "/static/media/trianglify.4c639dca.png"
+  },
+  {
+    "revision": "f4ca8f4df0cfc3257e953a31689d38c2",
+    "url": "/static/media/unidad-cinco.f4ca8f4d.png"
+  },
+  {
+    "revision": "2b72dd893b83e0fb129588b1fedc98c6",
+    "url": "/static/media/unidad-dos.2b72dd89.png"
+  },
+  {
+    "revision": "599c3d828ad39a8b1a8cc2d38a0f717c",
+    "url": "/static/media/unidad-tres.599c3d82.png"
+  },
+  {
+    "revision": "39eb2527eda0843c718b9373e83b3cc2",
+    "url": "/static/media/unidad-uno.39eb2527.png"
+  },
+  {
+    "revision": "ac488353052df277a5dceeb8de16d299",
+    "url": "/static/media/video_guia.ac488353.mp4"
+  }
+]);
